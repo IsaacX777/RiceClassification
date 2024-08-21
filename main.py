8930c4f4701f9c43cb725e4a6b4c14ae29fae6e6
IsaacX777 <isaacsun26711@gmail.com>
2024-08-20 19:47:32 -0500

    logistic regression model

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from scipy.io import arff
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import LabelEncoder
 
 data = arff.loadarff('Rice_Cammeo_Osmancik.arff')
 df = pd.DataFrame(data[0])
 
-y = df['Class']
+encoder = LabelEncoder()
+df['Class'] = encoder.fit_transform(df['Class'])
+
 x = df.drop('Class', axis=1)
+y = df['Class']
 
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 42)
 
-model = LinearRegression()
+model = LogisticRegression()
 model.fit(x_train, y_train)
+
+score = model.score(x_test, y_test)
